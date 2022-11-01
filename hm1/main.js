19b536189816d724a0ac0ae8d49123ed55aed711
kovalialina <alinakovalisin7@gmail.com>
2022-11-01 13:26:19 +0100

    hm1

---
+++ hm1/main.js
@@ -0,0 +1,38 @@
+let s1 = "hello"
+let s2 = "owu"
+let s3 = "com"
+let s4 = "ua"
+console.log(`${s1} ${s2} ${s3} ${s4}`)
+
+let n1 = 1
+let n2 = 10
+let n3 = -999
+let n4 = 123
+console.log(`${n1} ${n2} ${n3} ${n4}`)
+
+const PI = 3.14
+const A = 2.7
+console.log(`${PI} ${A}`)
+
+let n5 = 16
+console.log (n5)
+
+let b1 = 'true'
+let b2 = 'false'
+console.log(`${b1} ${b2}`)
+
+let firstName = 'Alina'
+let middleName = 'Kovalyshin'
+let lastName = 'Vasilyevna'
+
+let a = 100
+console.log(typeof (a))
+
+let b = '100'
+console.log(typeof (b + ''))
+
+let c = true
+console.log(typeof (c))
+
+
+// let a = 100; let b = '100'; let c = true
