3dd9a8fae7acb15119faa2c5af3fbbc5e925a441
kovalialina <alinakovalisin7@gmail.com>
2022-11-02 22:18:10 +0100

    hm1

---
+++ hm2/main.js
@@ -0,0 +1,50 @@
+let arr = ['hello', 'owu', 'com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16]
+console.log(arr)
+
+let book1 = {
+    title: 'abc',
+    pageCount: 20,
+    genre: 'novel'
+}
+let book2 = {
+    title: 'dfg',
+    pageCount: 40,
+    genre: 'novel'
+}
+let book3 = {
+    title: 'hij',
+    pageCount: 50,
+    genre: 'novel'
+}
+let author1 = {name: 'vasya', age: 40}
+let author2 = {name: 'anna', age: 44}
+let author3 = {name: 'anton', age: 50}
+
+let author = [
+    author1,
+    author2,
+    author3
+]
+let user = [
+    {name:'Vasya', username:'vasya2555', password:25555555},
+    {name:'Alexiy', username:'Alexiy213', password:4783722},
+    {name:'Anastasia', username:'Anastasia467', password:4747262},
+    {name:'Anatoly', username:'Anatoly', password:43934439902},
+    {name:'Andriy', username:'Andriy748493', password:5774393},
+    {name:'Ganna', username:'Ganna5494', password:837482281},
+    {name:'Antonina', username:'Antonina8594', password:468238937},
+    {name:'Valery', username:'Valery9594', password:574833},
+    {name:'Valentin', username:'Valentin4893', password:22345566},
+    {name:'Varvara', username:'Varvara4833', password:6859494}
+]
+console.log(user[0].password)
+console.log(user[1].password)
+console.log(user[2].password)
+console.log(user[3].password)
+console.log(user[4].password)
+console.log(user[5].password)
+console.log(user[6].password)
+console.log(user[7].password)
+console.log(user[8].password)
+console.log(user[9].password)
+
